11c8175cee1c362b26055e3d2310b63b9450f686
Nambativ Peter Nambativ <116292003+Nambativep@users.noreply.github.com>
2023-12-07 01:48:45 +0100

    Add files via upload

---
+++ Python_Face_Detection.py
@@ -0,0 +1,18 @@
+import cv2
+import numpy
+
+face_cascade = cv2.CascadeClassifier("haarcascade_frontalcatface_extended.xml")
+
+img = cv2.imread("CAT_FACE.png")
+
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+
+for (x, y, w, h) in faces:
+    cv2.rectangle(img, (x, y), (x + w, y + h), (225, 0, 0), 2)
+
+cv2.imshow('img', img)
+cv2.waitKey()
+
+cv2.imwrite("face_detect.png", img)
